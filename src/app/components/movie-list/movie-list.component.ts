96c0196f0d8f2a07bfbbe5eb9958198e32f7d47c
Sofiia Anenko <anenko.sofiia@gmail.com>
2024-06-09 00:58:44 +0300

    ME-2: fix lint errors with max lenc

--- src/app/components/movie-list/movie-list.component.ts
+++ src/app/components/movie-list/movie-list.component.ts
@@ -23,7 +23,8 @@ export class MovieListComponent {
       id: 823464,
       original_language: "en",
       original_title: "Godzilla x Kong: The New Empire",
-      overview: "Following their explosive showdown, Godzilla and Kong must reunite against a colossal undiscovered threat hidden within our world, challenging their very existence – and our own.",
+      overview: `Following their explosive showdown, Godzilla and Kong must reunite against a colossal undiscovered
+       threat hidden within our world, challenging their very existence – and our own.`,
       popularity: 1931.335,
       poster_path: "/z1p34vh7dEOnLDmyCrlUVLuoDzd.jpg",
       release_date: "2024-03-27",
@@ -42,7 +43,9 @@ export class MovieListComponent {
       id: 693134,
       original_language: "en",
       original_title: "Dune: Part Two",
-      overview: "Follow the mythic journey of Paul Atreides as he unites with Chani and the Fremen while on a path of revenge against the conspirators who destroyed his family. Facing a choice between the love of his life and the fate of the known universe, Paul endeavors to prevent a terrible future only he can foresee.",
+      overview: `Follow the mythic journey of Paul Atreides as he unites with Chani and the Fremen while on a path of
+       revenge against the conspirators who destroyed his family. Facing a choice between the love of his life and the
+        fate of the known universe, Paul endeavors to prevent a terrible future only he can foresee.`,
       popularity: 787.686,
       poster_path: "/1pdfLvkbY9ohJlCjQH2CZjjYVvJ.jpg",
       release_date: "2024-02-27",
@@ -64,7 +67,9 @@ export class MovieListComponent {
       id: 1011985,
       original_language: "en",
       original_title: "Kung Fu Panda 4",
-      overview: "Po is gearing up to become the spiritual leader of his Valley of Peace, but also needs someone to take his place as Dragon Warrior. As such, he will train a new kung fu practitioner for the spot and will encounter a villain called the Chameleon who conjures villains from the past.",
+      overview: `Po is gearing up to become the spiritual leader of his Valley of Peace, but also needs someone to
+       take his place as Dragon Warrior. As such, he will train a new kung fu practitioner for the spot and will
+       encounter a villain called the Chameleon who conjures villains from the past.`,
       popularity: 705.24,
       poster_path: "/kDp1vUBnMpe8ak4rjgl3cLELqjU.jpg",
       release_date: "2024-03-02",

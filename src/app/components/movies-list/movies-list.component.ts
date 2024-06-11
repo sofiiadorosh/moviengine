70af8e61be44bc7ea58c8d62fb9e34d54be12946
Sofiia Anenko <anenko.sofiia@gmail.com>
2024-06-11 13:55:38 +0300

    ME-2: refactor code according to comments

---
+++ src/app/components/movies-list/movies-list.component.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { MovieItemComponent } from "@components/movie-item/movie-item.component";
+import { Movie } from "@models/movie.model";
+import movies from "@assets/json/movies.json";
+
+@Component({
+  selector: "app-movies-list",
+  standalone: true,
+  imports: [CommonModule, MovieItemComponent],
+  templateUrl: "./movies-list.component.html",
+  styleUrl: "./movies-list.component.scss",
+})
+export class MoviesListComponent {
+  movies: Movie[] = movies;
+  favoriteMovies: Movie[] = [];
+  watchLaterMovies: Movie[] = [];
+
+  private addToList(list: Movie[], id: number) {
+    const movieInList = list.find((movie) => movie.id === id);
+    if (movieInList) return list;
+    const addedMovie = this.movies.find((movie) => movie.id === id);
+    if (addedMovie) {
+      return [...list, addedMovie];
+    }
+    return list;
+  }
+
+  onAddToFavorites(id: number) {
+    this.favoriteMovies = this.addToList(this.favoriteMovies, id);
+  }
+
+  onAddToWatchlist(id: number) {
+    this.watchLaterMovies = this.addToList(this.watchLaterMovies, id);
+  }
+}

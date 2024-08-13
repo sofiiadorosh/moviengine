80abe1231ac77fe197b8976b9d2cd4b2a528ef39
Sofiia Dorosh <dorosh.sofiya@gmail.com>
2024-08-14 00:50:16 +0300

    ME-9: add movie service test

--- src/app/services/movie/movie.service.spec.ts
+++ src/app/services/movie/movie.service.spec.ts
@@ -1,23 +1,68 @@
-import { HttpClientTestingModule } from "@angular/common/http/testing";
-import { TestBed } from "@angular/core/testing";
-import { provideMockStore } from "@ngrx/store/testing";
+import { HttpErrorResponse } from "@angular/common/http";
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
+import { getTestBed, TestBed } from "@angular/core/testing";
+import { movies } from "@constants/movies";
+import { environment } from "@environments/environment";
+import { StoreModule } from "@ngrx/store";
 
 import { MovieService } from "./movie.service";
 
 describe("MovieService", () => {
   let service: MovieService;
+  let httpTestingController: HttpTestingController;
+  let injector: TestBed;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      providers: [
-        provideMockStore({}),
+      imports: [
+        HttpClientTestingModule,
+        StoreModule.forRoot({})
       ],
+      providers: [MovieService]
     });
-    service = TestBed.inject(MovieService);
+
+    injector = getTestBed();
+    service = injector.inject(MovieService);
+    httpTestingController = injector.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it("should be created", () => {
     expect(service).toBeTruthy();
+  });
+
+  it("should return popular movies", (done) => {
+    const mockMovies = { results: movies };
+
+    service.getPopularMovies().subscribe((data) => {
+      expect(data).toEqual(mockMovies.results);
+      done();
+    });
+
+    const req = httpTestingController.expectOne(
+      `${environment.apiBaseUrl}/movie/popular?api_key=${environment.apiKey}`
+    );
+    expect(req.request.method).toEqual("GET");
+    req.flush(mockMovies);
+  });
+
+  it("should handle error when fetching popular movies", (done) => {
+    service.getPopularMovies().subscribe({
+      next: (data) => {
+        expect(data).toEqual([]);
+      },
+      error: (error: HttpErrorResponse) => {
+        expect(error).toBeTruthy();
+      }
+    });
+
+    const req = httpTestingController.expectOne(
+      `${environment.apiBaseUrl}/movie/popular?api_key=${environment.apiKey}`
+    );
+    req.flush(null, { status: 500, statusText: "Server Error" });
+    done();
   });
 });
